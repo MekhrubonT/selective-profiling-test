3dd1089fe13fd58b7675cbf413472ef7126d769a
MekhrubonT <mehrubon.97@mail.ru>
2017-05-09 07:34:32 +0300

    CallTree

--- src/org/jetbrains/test/Main.java
+++ src/org/jetbrains/test/Main.java
@@ -2,10 +2,13 @@ package org.jetbrains.test;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -14,22 +17,23 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException, IOException {
         ExecutorService service = Executors.newFixedThreadPool(3);
+        List<Future<?>> futures = new ArrayList<>();
         for(int i = 0; i < 5; i++) {
             int start = 100 * i;
             List<String> arguments = IntStream.range(start, start + 10)
                     .mapToObj(Integer :: toString)
                     .collect(Collectors.toList());
             Runnable r = () -> new DummyApplication(arguments).start();
-                        service.submit(r);
+            futures.add(service.submit(r));
         }
         service.shutdown();
-        service.awaitTermination(1000, TimeUnit.HOURS);
-        System.out.println("Shutdown");
-
-        try (PrintWriter writer = new PrintWriter(new File("output.txt"))) {
-            CallTree.getTrees().forEach(writer::println);
+        while (!service.isTerminated()) {
+            service.awaitTermination(1, TimeUnit.SECONDS);
         }
 
+        try (PrintWriter writer = new PrintWriter(new OutputStreamWriter(System.out))) {
+            CallTree.getTrees().forEach(writer::println);
+        }
         for (CallTree bypassTree : CallTree.getTrees()) {
             bypassTree.storeInFile();
         }
